528ddcfff0a6a2defa121a69bdb2f7c2c89f3fe3
Alessandro Paris <alessandro1.paris@epitech.eu>
2026-07-07 15:15:37 +0200

    feat: add collisions between the snake and the apples and add score update

--- src/game/game.py
+++ src/game/game.py
@@ -3,7 +3,7 @@ import sys
 import pygame
 from pygame.locals import *
  
-from src.config import WIDTH, HEIGHT, FPS, SNAKE_COLOR, SNAKE_X, SNAKE_Y, SNAKE_SIZE, SNAKE_SPEED, BLACK, NB_APPLES , APPLE_SIZE, APPLE_COLOR
+from src.config import WIDTH, HEIGHT, FPS, SNAKE_COLOR, SNAKE_X, SNAKE_Y, SNAKE_SIZE, SNAKE_SPEED, BLACK, NB_APPLES , APPLE_SIZE, APPLE_COLOR, WHITE, SCORE_POS
 
 from src.entities.snake import Snake
 from src.entities.apple import generateApple
@@ -13,10 +13,25 @@ pygame.init()
 fpsClock = pygame.time.Clock()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
+pygame.font.init()
+my_font = pygame.font.SysFont('Comic Sans MS', 50)
+
 class Game:
 
     def __init__(self):
         self.apples = [generateApple(APPLE_SIZE, APPLE_SIZE, APPLE_COLOR) for _ in range(NB_APPLES)]
+        self.score = my_font.render('Score: 0', False, WHITE)
+
+    def checkColisions(self):
+        snake_rect = pygame.Rect(*self.snake.rect)
+
+        for i in range(len(self.apples) - 1, -1, -1):
+            apple = self.apples[i]
+            apple_rect = pygame.Rect(*apple.rect)
+            if snake_rect.colliderect(apple_rect):
+                self.apples.pop(i)
+                self.snake.score += 1
+
 
     def update(self):
         """
@@ -24,12 +39,15 @@ class Game:
         OUTPUT: none
         Update the game
         """
-        pass
+        self.score = my_font.render('Score: ' + str(self.snake.score), False, WHITE)
+        self.checkColisions()
 
     def draw(self):
+        screen.fill(BLACK)
         self.snake.draw(screen)
         for apple in self.apples:
             apple.draw(screen)
+        screen.blit(self.score, SCORE_POS)
 
     def handleKeys(self):
         keys = pygame.key.get_pressed()
@@ -51,8 +69,6 @@ class Game:
         self.snake = Snake(SNAKE_COLOR, SNAKE_X, SNAKE_Y, SNAKE_SIZE, SNAKE_SIZE, SNAKE_SPEED)
 
         while True:
-            screen.fill(BLACK)
-            
             for event in pygame.event.get():
                 if event.type == QUIT:
                     pygame.quit()
@@ -61,7 +77,7 @@ class Game:
             self.handleKeys()
             self.update()
             self.draw()
-            
+
             pygame.display.flip()
             fpsClock.tick(FPS)
 
